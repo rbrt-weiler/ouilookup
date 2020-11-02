2b22b80b7d3fd96af32425d1ecf436e881b014ec
Robert WEILER <rbrt-weiler@mailbox.org>
2020-11-02 21:38:29 +0100

    Minor additions to server functionality.

--- cmd_server.go
+++ cmd_server.go
@@ -3,13 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	mux "github.com/gorilla/mux"
+)
+
+var (
+	persistentOUIDatabase    ouiDatabase
+	persistentVendorDatabase map[string][]string
 )
 
 func serverMain() {
 	devMessage("Entering serverMain()")
 	sanitizeArguments()
+
+	db, dbErr := loadDatabase(config.DatabaseFile)
+	if dbErr != nil {
+		stdErr.Printf("Error loading database: %s\n", dbErr)
+		os.Exit(errDatabaseLoad)
+	}
+	vendorDB, vendorDBErr := ouiToVendorDatabase(db)
+	if vendorDBErr != nil {
+		stdErr.Printf("Error converting database: %s\n", vendorDBErr)
+		os.Exit(errDatabaseConvert)
+	}
+	persistentOUIDatabase = db
+	persistentVendorDatabase = vendorDB
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", handlerRoot)
@@ -19,5 +38,6 @@ func serverMain() {
 }
 
 func handlerRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", toolID)
+	fmt.Fprintf(w, "%s\n", toolID)
+	fmt.Fprintf(w, "%d unique OUIs and %d unique vendors in database\n", len(persistentOUIDatabase.OUIDatabase), len(persistentVendorDatabase))
 }
